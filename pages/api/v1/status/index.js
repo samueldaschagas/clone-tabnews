58780fbf49c0c5264db25004d409ab780397a0f3
Samuel das Chagas <samueldaschagas@gmail.com>
2023-12-02 01:57:20 +0000

    Finaliza endpoint  e  ajusta testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,8 @@ import database from "infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1;");
   console.log(result.rows);
-  response.status(200).json({ chave: "Valor" });
+  const updateAt = new Date().toISOString();
+  response.status(200).json({ update_at: updateAt });
 }
 
 export default status;
